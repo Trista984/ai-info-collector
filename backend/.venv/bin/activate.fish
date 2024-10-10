e9e5e6149323dc73f5835bc6e6978401832b264e
赵艺璇 <905232526@qq.com>
2024-10-10 16:58:10 -0400

    Initial commit after fixing dependencies

--- backend/.venv/bin/activate.fish
+++ backend/.venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/zhaoyixuan/Desktop/ai-info-collector/backend/.venv"
+set -gx VIRTUAL_ENV "/Users/zhaoyixuan/ai-info-collector/backend/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
